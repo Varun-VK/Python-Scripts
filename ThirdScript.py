5a6ebe62c2dadbcd5d542c0e3510060797f77562
Varun Konduru <varun.usa7@gmail.com>
2016-10-31 11:15:28 -0400

    Third Script added - Register a User.

---
+++ ThirdScript.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import unittest
+
+class Test_PythonOrg(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		sleep(4)
+		self.browser.close()
+		self.browser = None
+    
+    def test_03_RegisterAUserWorks(self):
+		self.browser.get("http://www.ebay.com")
+		link = self.browser.find_element_by_link_text('register')
+		link.click()
+		email = self.browser.find_element_by_id("email")
+		reEmail = self.browser.find_element_by_id("remail")
+		password = self.browser.find_element_by_id("PASSWORD")
+		firstName = self.browser.find_element_by_id("firstname")
+		lastName = self.browser.find_element_by_id("lastname")
+		mobile = self.browser.find_element_by_id("phoneFlagComp1")
+		email.send_keys("vkonduru@kent.edu")
+		reEmail.send_keys("vkonduru@kent.edu")
+		password.send_keys("123456789")
+		firstName.send_keys("Varun")
+		lastName.send_keys("Konduru")
+		mobile.send_keys("2347163446")
+		self.browser.find_element_by_id("sbtBtn").click()
+		self.assertTrue("No results found." not in self.browser.page_source)
+    
+if __name__ == "__main__":
+	unittest.main(verbosity=2)
